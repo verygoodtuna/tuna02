9ef355b8f3119139884ebce4c63e997db2c7c8e5
verygoodtuna <133613555+verygoodtuna@users.noreply.github.com>
2023-05-15 18:14:05 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    const option = {
+        anchors: ['01', '02', '03', '04', '05'],
+
+
+    }
+
+    $('.main_full').fullpage(option);
+
+    $('.section').eq(0).addClass('on');
+
+})
